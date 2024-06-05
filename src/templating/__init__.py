f838d433bbf24f22aa11234c58b2b15c1031f1bb
Troy Kelly <troy@troykelly.com>
2024-06-05 17:11:34 +1000

    Adding Jinja2

---
+++ src/templating/__init__.py
@@ -0,0 +1,6 @@
+# src/templating/__init__.py
+"""
+Templating module for integrating Jinja2 templating with additional functionalities.
+"""
+
+from .handlers import TemplateHandlers, render_template
